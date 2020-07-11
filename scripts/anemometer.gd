d3ba88dc30c9f9223ab47a8dcead88bb9cf74e17
Steven Dwy <yoplitein+git@gmail.com>
2020-07-11 02:38:01 -0700

    Mark anemometer script as tool so it will render in editor

--- scripts/anemometer.gd
+++ scripts/anemometer.gd
@@ -1,3 +1,4 @@
+tool
 extends Control
 
 export var windAngle = 0 setget _set_angle
@@ -9,8 +10,6 @@ onready var speedLabel = get_parent().find_node("WindSpeed")
 func _draw():
 	var radAng = deg2rad(windAngle)
 	var lineEnd = diameter / 2 * Vector2(cos(radAng), sin(radAng))
-	
-	print(radAng, lineEnd, windAngle)
 	
 	draw_circle(Vector2(0, 0), diameter / 2, Color(0x454545FF))
 	draw_line(Vector2(0, 0), lineEnd, Color(1, 0, 0), 1, true)
