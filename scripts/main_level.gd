4c5bf2ea567d88040f98ff6f0bca93dee99b9fdc
narcolembia <masterlybeatle@gmail.com>
2020-07-11 11:49:59 -0700

    nearly empty commit for posterity before branching

--- scripts/main_level.gd
+++ scripts/main_level.gd
@@ -6,31 +6,40 @@ var tiles: Array = [] # 2d array, [y][x]
 onready var worldMap: TileMap = get_node("worldMap") #TileMap node which we are using to render
 onready var fireMap: TileMap = get_node("fireMap") #TileMap of the fire that will be drawn over the world
 onready var truck = get_node("Fire Truck")
+var fireSpreadTime = 1 #timeInSeconds
 
 # Returns -1 for non-traversable tiles and out of bounds positions
 # Otherwise, returns the movementCost of the tile at the given position
+# Param: pos - Vector2 - a position on the tile grid to be checked
 func tileMoveCost(pos):
-	if(pos.x >= size.x || pos.x < 0 || pos.y >= size.y || pos.y < 0): # are we out of the grid?
+	if tileInbounds(pos): # are we out of the grid?
 		return -1
 	else:
-		#return 1
 		return tiles[pos.y][pos.x].movementCost
 
-func tileInbounds(tilePos):
-	return worldMap.get_cellv(tilePos) != -1
 
+func tileInbounds(tilePos):
+	return worldMap.get_cellv(tilePos) != TileMap.INVALID_CELL
+
+# Returns the position of the tile in the grid that contains the given coordinates
+# Param: tilePos - Vector2
+# Return: Vector2
 func tileToWorld(tilePos):
 	var retCoord = worldMap.map_to_world(tilePos)
 	retCoord.x +=cellSize/2
 	retCoord.y +=cellSize/2
 	return retCoord
+
+# Returns the coordinates of the center of the tile at the given position
+# Param: worldPos - Vector2
+# Return: Vector2
 func worldToTile(worldPos):
 	return worldMap.world_to_map(worldPos)
 
 # Constructs a new internal tile object matching the tile in gridMap at the given position
 func getInternalTile(tileMap, pos, nameMap):
 	var id = tileMap.get_cellv(pos)
-	#assert(id != 0, "someone should really make sure that the map has no holes in it")
+	assert(id != -1, "someone should really make sure that the map has no holes in it")
 	var tile = Internal_Tile.new(nameMap[id])
 	tile.pos = pos
 	return tile
@@ -56,6 +65,20 @@ func _ready():
 			row.append(tile)
 		tiles.append(row)
 	truck.customInit()
+
+func fireSpread():
+	
+	var nextFireLevelsArr = [] #2d arr
+	for y in range(size.y):
+		var row = []
+		for x in range(size.x):
+			var fireStats = [tiles[y][x].fireLevel, tiles[y][x].fireResistance]
+			row.append(fireStats)
+			nextFireLevelsArr.append(row)
+	
+	for y in range(size.y):
+		for x in range(size.x):
+			
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
